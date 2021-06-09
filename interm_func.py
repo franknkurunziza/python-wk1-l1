a25ea330278a81b4a3ead5b11fe2acd0721a8033
Frank Nkurunziza <franknkurunzia@gmail.com>
2021-06-09 11:08:28 -0400

    first commit on intermediate function

---
+++ interm_func.py
@@ -0,0 +1,8 @@
+import random
+def randInt(max=100,min=0):
+    num=max-min
+    return round(random.random() * num+ min)
+print(randInt())
+print(randInt(max=50))
+print(randInt(min=50))
+print(randInt(min=50,max=100))
